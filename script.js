bb5c0c13fba3856ea148814b4e75c6d5a377a982
momosoni <momosoni@MOMOSONI-LAP.in.oracle.com>
2025-06-07 21:46:49 +0530

    change

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function playSound() {
-  const audio = document.getElementById('dingSound');
-  audio.play();
+  const audio = document.getElementById("dingSound");
+  audio.play().catch((error) => {
+    console.error("Audio playback failed:", error);
+  });
 }
